b527892606efad00e59d31564a5a43945b8e47bd
esmeraldo17 <esmeraldoalberto55@gmail.com>
2023-03-30 22:06:52 +0100

    fix: retorno no controller

--- back-end/src/controller/product.controller.js
+++ back-end/src/controller/product.controller.js
@@ -3,7 +3,7 @@ const { productService } = require('../service');
 const get = async (_req, res) => {
   const products = await productService.get();
 
-  res.status(200).json({ products });
+  res.status(200).json(products);
 };
 
 module.exports = {
