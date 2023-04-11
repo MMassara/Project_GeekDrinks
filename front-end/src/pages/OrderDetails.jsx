4cca7cbc2123f2991a1b1c5a831f90d3942e9122
Mmassara <matheus.massara@hotmail.com>
2023-04-11 17:48:07 -0300

    Custom NavBar

--- front-end/src/pages/OrderDetails.jsx
+++ front-end/src/pages/OrderDetails.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import NavBar from '../components/NavBar';
+import NavBar from '../components/NavBar/NavBar';
 import TableOrders from '../components/TableOrders';
 import dataTestsIds from '../utils/dataTestIds';
 
