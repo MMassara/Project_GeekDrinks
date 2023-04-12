fe672e47828923b88cfc63f4f03ae7963861587b
Jefferson <jefferson.felix182@gmail.com>
2023-04-12 18:08:48 -0300

    correcao do bug ao dar F5 apaga localstorage

--- front-end/src/pages/Cart.jsx
+++ front-end/src/pages/Cart.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
 import api, { setToken } from '../axios/config';
 import AddressForm from '../components/Cart/AddressForm';
 import Countdown from '../components/Cart/Countdown';
 import Table from '../components/Cart/Table';
 import Navbar from '../components/NavBar/NavBar';
 import calcTotalPrice from '../utils/calcTotalPrice';
-import styled from 'styled-components';
 
 const Main = styled.section`
     background-color: #FFF3E0;
@@ -98,7 +98,6 @@ export default function Cart() {
             />
           </section>
         </>
-        
       )}
     </Main>
   );
